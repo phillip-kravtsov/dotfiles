f3c22e15f5b6e3b5ea519f2046c9ae8fd1669607
Phillip Kravtsov <phillip@kravtsov.net>
2023-10-31 20:17:14 +0000

    add git config

--- setup.sh
+++ setup.sh
@@ -3,4 +3,7 @@ cp .vimrc ~/.vimrc
 cp .zshrc ~/.zshrc
 git clone https://github.com/scmbreeze/scm_breeze.git ~/.scm_breeze
 ~/.scm_breeze/install.sh
+git config --global user.name "Phillip Kravtsov"
+git config --global user.email "phillip@kravtsov.net"
+git config --global core.editor vim
 
