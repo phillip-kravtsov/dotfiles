138300d0076efa2986af05aab6e8c79df0fccfef
Phillip Kravtsov <phillip@trucksmarter.com>
2023-11-03 10:55:29 -0700

    update bench

--- bench.sh
+++ bench.sh
@@ -1,4 +1,4 @@
-#wget https://raw.githubusercontent.com/stas00/ml-engineering/master/multi-node/all_reduce_bench.py
+wget https://raw.githubusercontent.com/stas00/ml-engineering/master/multi-node/all_reduce_bench.py
 export NNODES=1
 export GPUS_PER_NODE=4
-torchrun --nproc_per_node $GPUS_PER_NODE --nnodes $NNODES --max_restarts 0 --tee 3 ../all_reduce_bench.py
+torchrun --nproc_per_node $GPUS_PER_NODE --nnodes $NNODES --max_restarts 0 --tee 3 all_reduce_bench.py
