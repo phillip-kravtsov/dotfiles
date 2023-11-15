92e03f8810e063da70dd89452b38e734c129eeb0
Phillip Kravtsov <phillip@kravtsov.net>
2023-11-15 19:03:29 +0000

    add startu

---
+++ startup.sh
@@ -0,0 +1,4 @@
+apt-get install -y awscli
+wget https://github.com/peak/s5cmd/releases/download/v2.2.2/s5cmd_2.2.2_Linux-64bit.tar.gz
+tar -xvf s5cmd_2.2.2_Linux-64bit.tar.gz
+mv s5cmd /usr/local/bin
