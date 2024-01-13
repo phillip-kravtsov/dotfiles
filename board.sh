545a1918253913f35dea342d05308d5af666f5fd
Phillip Kravtsov <phillip@kravtsov.net>
2024-01-13 19:55:06 +0000

    security

--- board.sh
+++ board.sh
@@ -1,3 +1,3 @@
-ssh-keygen -t ed25519 -C "phillip@kravtsov.net" -N '' -f ~/.ssh/id_ed25519 <<<y >/dev/null 2>&1
+ssh-keygen -t ed25519 -C "phillip@kravtsov.net" -N "$PASSWORD" -f ~/.ssh/id_ed25519 <<<y >/dev/null 2>&1
 eval "$(ssh-agent -s)" && ssh-add ~/.ssh/id_ed25519 && cat ~/.ssh/id_ed25519.pub
 echo "https://github.com/settings/ssh/new"
