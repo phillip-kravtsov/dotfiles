e1e1ccf1665ef5fb89008fa58e60986c17a79e42
Phillip Kravtsov <phillip@kravtsov.net>
2024-01-13 19:52:31 +0000

    bingus

--- board.sh
+++ board.sh
@@ -1,6 +1,4 @@
 ssh-keygen -t ed25519 -C "phillip@kravtsov.net" -N '' -f ~/.ssh/id_ed25519 <<<y >/dev/null 2>&1
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_ed25519
-cat ~/.ssh/id_ed25519.pub
+eval "$(ssh-agent -s)" && ssh-add ~/.ssh/id_ed25519 && cat ~/.ssh/id_ed25519.pub
 echo "https://github.com/settings/ssh/new"
 echo "git clone git@github.com:phillip-kravtsov/monorepo.git"
