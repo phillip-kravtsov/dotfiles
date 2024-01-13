86bf95f6c287db6c474f672900d09227553c8252
Phillip Kravtsov <phillip@kravtsov.net>
2024-01-13 19:55:42 +0000

    add another classic

--- board.sh
+++ board.sh
@@ -1,3 +1,4 @@
 ssh-keygen -t ed25519 -C "phillip@kravtsov.net" -N "$PASSWORD" -f ~/.ssh/id_ed25519 <<<y >/dev/null 2>&1
 eval "$(ssh-agent -s)" && ssh-add ~/.ssh/id_ed25519 && cat ~/.ssh/id_ed25519.pub
 echo "https://github.com/settings/ssh/new"
+git remote add s git@github.com:phillip-kravtsov/dotfiles.git
